1f9cc68c19067c867f2c5d5f090604773a13eb5e
yjyoon-dev <yjyoon.dev@gmail.com>
2023-01-23 21:45:28 +0900

    fix: dependecies 추가 방식을 version catalog로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
-plugins {
-    id("com.android.application") version "7.4.0" apply false
-    id("com.android.library") version "7.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.7.20" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(libs.android.gradle)
+        classpath(libs.kotlin.gradle)
+    }
 }
