6a53d137cca9c902ba4a21c055f5053b6abaa647
Jisun Park <pjs5221@naver.com>
2023-01-28 15:54:40 +0900

    chore:  카카오 sdk 및 secret-gradle 의존성 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,14 +5,18 @@ pluginManagement {
         mavenCentral()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
+
 rootProject.name = "PIMO"
+
 include(
     ":app",
     ":domain",
