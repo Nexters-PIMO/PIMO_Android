d594e4884f2a89203d94deb54e2d9ce9cb1aa676
yjyoon-dev <yjyoon.dev@gmail.com>
2023-01-31 05:17:04 +0900

    feat: MainActivity에서 Dummy Json 테스트 기능 추가

--- presentation/src/main/java/com/nexters/pimo/ui/MainActivity.kt
+++ presentation/src/main/java/com/nexters/pimo/ui/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.nexters.pimo.ui.theme.PIMOTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,9 +25,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             PIMOTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    contentAlignment = Alignment.Center
                 ) {
                     DummyButton(viewModel::onDummyButtonClick)
                 }
