dac7fd15b1ea3d4f254c1fc3b2e32ce78a7ff596
yjyoon-dev <yjyoon.dev@gmail.com>
2023-01-31 01:00:36 +0900

    refactor: import 시 wild card를 사용하지 않도록 수정

--- remote/src/main/java/com/nexters/pimo/remote/source/DummyDataSourceImpl.kt
+++ remote/src/main/java/com/nexters/pimo/remote/source/DummyDataSourceImpl.kt
@@ -3,9 +3,9 @@ package com.nexters.pimo.remote.source
 import com.nexters.pimo.data.model.DummyJsonEntity
 import com.nexters.pimo.data.source.DummyDataSource
 import com.nexters.pimo.remote.model.DummyJsonResponse
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.request.*
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 import javax.inject.Inject
 
 internal class DummyDataSourceImpl @Inject constructor(
