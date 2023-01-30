c6f613d13a0cb4ff0087027d23e7fda049bd8306
yjyoon-dev <yjyoon.dev@gmail.com>
2023-01-31 05:21:26 +0900

    refactor: 불필요한 import 제거

--- presentation/src/main/java/com/nexters/pimo/ui/MainActivity.kt
+++ presentation/src/main/java/com/nexters/pimo/ui/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
