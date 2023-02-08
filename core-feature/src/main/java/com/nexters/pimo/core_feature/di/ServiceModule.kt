0b5110813ac56086405579162be818685428227c
yjyoon-dev <yjyoon.dev@gmail.com>
2023-02-09 05:06:02 +0900

    feat: TTS 기능 구현 및 모듈화

--- core-feature/src/main/java/com/nexters/pimo/core_feature/di/ServiceModule.kt
+++ core-feature/src/main/java/com/nexters/pimo/core_feature/di/ServiceModule.kt
@@ -1,7 +1,9 @@
 package com.nexters.pimo.core_feature.di
 
 import com.nexters.pimo.core_feature.ocr.OcrServiceImpl
+import com.nexters.pimo.core_feature.tts.TtsServiceImpl
 import com.nexters.pimo.feature.ocr.OcrService
+import com.nexters.pimo.feature.tts.TtsService
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,4 +17,8 @@ internal interface ServiceModule {
     @Binds
     @Singleton
     fun bindOcrService(service: OcrServiceImpl): OcrService
+
+    @Binds
+    @Singleton
+    fun bindTtsService(service: TtsServiceImpl): TtsService
 }
