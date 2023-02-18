17190bd39efe3cf73aa76cac3c80b168ed55a57f
Jisun Park <pjs5221@naver.com>
2023-02-18 15:03:20 +0900

    feat: 로그인 화면 구현

--- presentation/src/main/java/com/nexters/pimo/ui/login/LoginScreen.kt
+++ presentation/src/main/java/com/nexters/pimo/ui/login/LoginScreen.kt
@@ -1,12 +1,9 @@
 package com.nexters.pimo.ui.login
 
-import android.content.res.Resources.Theme
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,57 +11,67 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nexters.pimo.ui.R
+import com.nexters.pimo.ui.theme.FimoTheme
 
 
 @Composable
 fun LoginScreen(onLogin: () -> Unit) {
-    Column(
-        modifier = Modifier.fillMaxSize()
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(painterResource(id = R.drawable.bg_login), contentScale = ContentScale.FillHeight)
     ) {
-        LoginGraphic(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-        )
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .background(color = Color.White)
-                .fillMaxWidth()
-                .height(192.dp)
-                .padding(start = 24.dp, top = 60.dp, end = 24.dp, bottom = 48.dp)
+            modifier = Modifier.align(Alignment.TopCenter),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Spacer(modifier = Modifier.height(190.dp))
+            Text(
+                text = stringResource(id = R.string.login_title),
+                style = FimoTheme.typography.light.copy(
+                    fontSize = 16.sp,
+                    color = FimoTheme.colors.white
+                ),
+                textAlign = TextAlign.Center,
+            )
+            Spacer(modifier = Modifier.height(25.dp))
+            Image(
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(40.dp),
+                painter = painterResource(id = R.drawable.img_logo_login),
+                contentDescription = null,
+            )
+        }
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.signup_sns),
+                style = FimoTheme.typography.medium.copy(
+                    fontSize = 14.sp,
+                    color = FimoTheme.colors.white
+                ),
+                textAlign = TextAlign.Center,
+            )
+            Spacer(modifier = Modifier.height(20.dp))
             KakaoLoginButton(onClick = onLogin)
         }
     }
-}
 
-@Composable
-fun LoginGraphic(modifier: Modifier = Modifier) {
-    Surface(
-        color = Color.White,
-        modifier = modifier
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(36.dp, Alignment.CenterVertically),
-            modifier = Modifier.padding(horizontal = 24.dp)
-        ) {
-
-        }
-    }
 }
 
 @Composable
@@ -75,6 +82,7 @@ fun KakaoLoginButton(onClick: () -> Unit) {
         modifier = Modifier
             .clip(RoundedCornerShape(12.dp))
             .clickable { onClick() }
+            .height(54.dp)
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -86,15 +94,16 @@ fun KakaoLoginButton(onClick: () -> Unit) {
                 tint = Color.Black,
                 modifier = Modifier.size(24.dp)
             )
-            //HorizontalSpacer(width = 12.dp)
             Text(
-                text = stringResource(R.string.login_kakao),
-                color = Color.Black.copy(alpha = 0.85f),
+                text = stringResource(id = R.string.login_kakao),
+                style = FimoTheme.typography.medium.copy(
+                    fontSize = 18.sp,
+                    color = FimoTheme.colors.black
+                ),
                 textAlign = TextAlign.Center,
                 modifier = Modifier.weight(1f)
             )
         }
     }
 }
-
 
