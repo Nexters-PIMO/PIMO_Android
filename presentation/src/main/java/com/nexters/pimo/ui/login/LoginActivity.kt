c272d3309e7c18b261f084f237c94a4a683fb12b
Jisun Park <pjs5221@naver.com>
2023-02-19 15:01:31 +0900

    feat: 로그인->온보딩 화면 전환

--- presentation/src/main/java/com/nexters/pimo/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/nexters/pimo/ui/login/LoginActivity.kt
@@ -3,11 +3,17 @@ package com.nexters.pimo.ui.login
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.nexters.pimo.domain.model.LoginResult
 import com.nexters.pimo.feature.kakao.KakaoLogin
 import com.nexters.pimo.ui.base.BaseActivity
+import com.nexters.pimo.ui.main.MainActivity
+import com.nexters.pimo.ui.onboard.OnboardActivity
+
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.viewmodel.observe
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -16,21 +22,50 @@ class LoginActivity : BaseActivity() {
     @Inject
     lateinit var kakaoLogin: KakaoLogin
 
+    private val loginViewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
+        loginViewModel.observe(lifecycleOwner = this, state = ::handleState, sideEffect = ::handleSideEffect)
+
+        setContent{
             LoginScreen(
                 onLogin = { requestKakaoLogin() }
             )
         }
     }
 
+    private fun handleState(state: LoginState) {
+        when (state.result) {
+            LoginResult.Signed -> startMainActivity()
+            LoginResult.Onboard -> startOnboardActivity()
+            else -> Unit
+        }
+    }
+
+    private fun startOnboardActivity() {
+        OnboardActivity.startActivity(this)
+        finish()
+    }
+
+    private fun startMainActivity() {
+//        MainActivity.startActivity(this)
+//        finish()
+    }
+
+    private fun handleSideEffect(sideEffect: LoginSideEffect) {
+        when (sideEffect) {
+            is LoginSideEffect.Toast -> LoginSideEffect.Toast(sideEffect.text)
+        }
+    }
+
     private fun requestKakaoLogin() = lifecycleScope.launch {
         kakaoLogin.login(this@LoginActivity)
             .onSuccess {
-                Log.d("accessToken: ", it.kakaoAccessToken)
-                Log.d("refreshToken: ", it.kakaoRefreshToken)
+//                Log.d("accessToken: ", it.kakaoAccessToken)
+//                Log.d("refreshToken: ", it.kakaoRefreshToken)
+                loginViewModel.login(it.kakaoAccessToken, it.kakaoRefreshToken)
             }
             .onFailure {
                 handleException(it)
