3c36ddfee950270259fe5aaf5610bbe15a24812e
Jisun Park <pjs5221@naver.com>
2023-03-02 22:37:28 +0900

    feat: 온보딩 화면 전환 모드 추가(로그인O/X)

---
+++ presentation/src/main/java/com/nexters/pimo/ui/onboard/OnboardViewModel.kt
@@ -0,0 +1,35 @@
+package com.nexters.pimo.ui.onboard
+
+import androidx.lifecycle.SavedStateHandle
+import com.nexters.pimo.ui.base.BaseViewModel
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
+
+class OnboardViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    ) : ContainerHost<OnboardState, OnboardSideEffect>,
+    BaseViewModel() {
+
+    override val container = container<OnboardState, OnboardSideEffect>(OnboardState())
+
+    private val isLogin: Boolean = savedStateHandle.get<Boolean>(KEY_MODE)!!
+
+    init {
+        intent {
+            reduce {
+                state.copy(
+                    isLogin = isLogin,
+                )
+            }
+        }
+    }
+
+    companion object {
+        const val KEY_MODE = "KEY_MODE"
+    }
+
+}
+
